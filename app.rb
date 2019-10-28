24316edbfd718c1233b70bc04b8e5b3854667747
Rachid Rakaa <rachid1982fsb@gmail.com>
2019-10-28 14:35:48 -0700

    Done.

--- app.rb
+++ app.rb
@@ -3,8 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Rachid Rakaa Welcome to your app!!!! 
-    try"
+    "Rachid Rakaa Welcome to your app!!!!"
 
   end
 
